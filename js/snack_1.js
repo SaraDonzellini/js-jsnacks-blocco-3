7e22e3f54290dc31a42f1299dff0cbe627335601
SaraDonzellini <saradonzie@gmail.com>
2022-11-28 16:44:13 +0100

    snack 1 & 2

---
+++ js/snack_1.js
@@ -0,0 +1,10 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+let userNumber = parseInt(prompt("inserisci un numero da 1 a 10"));
+
+while (userNumber < 1 || userNumber > 10){
+  userNumber = parseInt(prompt("inserisci un numero da 1 a 10"));
+  console.log(userNumber);
+}
+
+// Number.isNaN || userNumber < 1 || userNumber > 10
+
---
+++ js/snack_2.js
@@ -0,0 +1,22 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+
+let arrayNums = [];
+
+let userNumber = parseInt(prompt("Inserisci un numero minore di 50"));
+let somma = 0;
+
+
+while (somma <= 50) {
+  userNumber = parseInt(prompt("Inserisci un numero minore di 50"));
+  somma += userNumber
+  arrayNums.push(userNumber)
+
+  if (somma > 50){
+    console.log('hai sballato')
+  }
+
+}
+console.log(somma)
+console.log(arrayNums)
+console.log(userNumber)
