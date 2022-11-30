e0b7725c3f3d8aba68e5059db6cb5f4f40fae0b5
SaraDonzellini <saradonzie@gmail.com>
2022-11-30 11:42:58 +0100

    megasnack_1

---
+++ js/megasnack_1.js
@@ -0,0 +1,36 @@
+/**
+* Crea due tag div con due id diversi.
+* Un div avrà il testo colorato di rosso mentre l'altro di verde.
+* Partendo da un array di numeri,
+* creiamo un nuovo elemento p e lo stampiamo nell'div rosso  se è un numero dispari
+* e nel div verde se è un numero pari.
+*
+*
+*/
+
+// funzione che crei un nuovo elemento <p> </p>
+// restituirlo a chi invoca la funzione
+//
+// ciclo per ogni elemento dell'array
+// se l'elemento è un numero pari, crei nuovo elemento e lo aggiungo al div verde
+// se l'elemento è un numero dispari, creo  nuovo elemento e lo aggiungo al div rosso
+const array = [1, 33, 55, 64, 33, 22, 10, 44];
+const divVerde = document.querySelector(".verde");
+const divRosso = document.querySelector(".rosso");
+
+function newPElement (p) {
+  const PElement = document.createElement('p');
+  
+  return;
+}
+
+for (let i = 0; i < array.length; i++) {
+  
+  if (array[i] % 2 === 0){
+    divVerde.append(newPElement(i) + " ");
+  } else {
+    divRosso.append(newPElement(i) + " ");
+  }
+
+  
+}
